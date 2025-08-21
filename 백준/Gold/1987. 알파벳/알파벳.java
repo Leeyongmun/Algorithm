1ed695f47d0a2d37a6089ae5e8023b388ad2c406
Leeyongmun <cocoyi000319@gmail.com>
2025-08-21 19:35:30 +0900

    [Gold IV] Title: 알파벳, Time: 884 ms, Memory: 18232 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static char[][] a;
+    static int mask = 0;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int ret = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = new char[n][m];
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            for(int j = 0; j < m; j++){
+                a[i][j] = s.charAt(j);
+            }
+        }
+        mask |= (1 << (a[0][0] - 'A'));
+        dfs(0, 0, 1);
+        System.out.println(ret);
+    }
+    
+    private static void dfs(int y, int x, int cnt){
+        if(cnt > ret) ret = cnt;
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if((mask & (1 << (a[ny][nx] - 'A'))) != 0) continue;
+            mask |= (1 << (a[ny][nx] - 'A'));
+            dfs(ny, nx, cnt + 1);
+            mask &= ~(1 << (a[ny][nx] - 'A'));
+        }
+    }
+}

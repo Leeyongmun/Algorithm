d73d901470d8fecfd440fafbf729c8ec0dc61b67
Leeyongmun <cocoyi000319@gmail.com>
2025-08-31 21:27:19 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3944 ms, Memory: 398608 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,62 +1,63 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     static int n, m;
-    static int[] parent;
-    static Edge[] edge;
+    static ArrayList<Edge>[] graph;
+    static int[] minEdge;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        parent = new int[n + 1];
-        edge = new Edge[m];
+        graph = new ArrayList[n + 1];
         for(int i = 1; i <= n; i++){
-            parent[i] = i;
+            graph[i] = new ArrayList<>();
         }
+        minEdge = new int[n + 1];
+        Arrays.fill(minEdge, 987654321);
         for(int i = 0; i < m; i++){
             int from = sc.nextInt();
             int to = sc.nextInt();
             int cost = sc.nextInt();
-            edge[i] = new Edge(from, to, cost);
+            graph[from].add(new Edge(to, cost));
+            graph[to].add(new Edge(from, cost));
         }
-        Arrays.sort(edge, Comparator.comparingInt(e -> e.cost));
-
+        boolean[] visited = new boolean[n + 1];
+        minEdge[1] = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.cost));
+        pq.offer(new Edge(1, 0));
         int cnt = 0;
         int ret = 0;
         int max = -1;
-        for(int i = 0; i < m; i++){
-            int a = edge[i].from;
-            int b = edge[i].to;
-            int w = edge[i].cost;
-            if(!merge(a, b)) continue;
+        while(!pq.isEmpty() && cnt < n){
+            Edge now = pq.poll();
+            int cur = now.to;
+            int cost = now.cost;
+            if(visited[cur]) continue;
+            visited[cur] = true;
+            max = Math.max(max, cost);
+            ret += cost;
             cnt++;
-            ret += w;
-            max = Math.max(max, w);
-            if(cnt == n - 1) break;
+            for(Edge p : graph[cur]){
+                int next = p.to;
+                int w = p.cost;
+                if(!visited[next] && minEdge[next] > w){
+                    minEdge[next] = w;
+                    pq.offer(p);
+                }
+            }
         }
         System.out.println(ret - max);
     }
 
-    private static int find(int x){
-        if(x == parent[x]) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    private static boolean merge(int x, int y){
-        x = find(x);
-        y = find(y);
-        if(x == y) return false;
-        parent[x] = y;
-        return true;
-    }
-
     static class Edge{
-        int from, to, cost;
-        Edge(int from, int to, int cost){
-            this.from = from;
+        int to, cost;
+        Edge(int to, int cost){
             this.to = to;
             this.cost = cost;
         }

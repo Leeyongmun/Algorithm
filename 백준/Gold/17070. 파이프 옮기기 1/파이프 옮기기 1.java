fd0ce0f4d98ee2e89b1df711f6e75b0801262a43
Leeyongmun <cocoyi000319@gmail.com>
2025-09-07 09:20:07 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 13460 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[][] a;
+    static int[][][] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        a = new int[17][17];
+        dp = new int[3][17][17];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+        dp[0][1][2] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 3; j <= n; j++){
+                if(a[i][j] == 1) continue;
+                dp[0][i][j] = dp[0][i][j-1] + dp[2][i][j-1];
+                dp[1][i][j] = dp[1][i-1][j] + dp[2][i-1][j];
+                if(a[i-1][j] == 1 || a[i][j-1] == 1) continue;
+                dp[2][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1];
+            }
+        }
+        System.out.println(dp[0][n][n] + dp[1][n][n] + dp[2][n][n]);
+    }
+}

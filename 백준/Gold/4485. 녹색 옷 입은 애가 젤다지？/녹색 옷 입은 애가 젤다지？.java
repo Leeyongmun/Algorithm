8003140fd1fbc3a1c4c9b699e948533fc9cc1555
Leeyongmun <cocoyi000319@gmail.com>
2025-09-13 09:57:05 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 268 ms, Memory: 21372 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,35 +1,38 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int T, n;
+    static int n;
     static int[][] a;
     static int[][] dist;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for(int t = 1; ; t++){
-            n = sc.nextInt();
+            n = Integer.parseInt(br.readLine());
             if(n == 0) return;
             a = new int[n][n];
             dist = new int[n][n];
             for(int i = 0; i < n; i++){
-                Arrays.fill(dist[i], 987654321);
-            }
-            for(int i = 0; i < n; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < n; j++){
-                    a[i][j] = sc.nextInt();
+                    a[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            PriorityQueue<Data> pq = new PriorityQueue<>(Comparator.comparing(e -> e.w));
-            pq.offer(new Data(0, 0, a[0][0]));
+            for(int i = 0; i < n; i++){
+                Arrays.fill(dist[i], 987654321);
+            }
+            PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(e -> e.w));
+            pq.offer(new Node(0, 0, a[0][0]));
             while(!pq.isEmpty()){
-                Data cur = pq.poll();
+                Node cur = pq.poll();
                 int y = cur.y;
                 int x = cur.x;
                 int w = cur.w;
@@ -38,10 +41,10 @@ public class Main {
                     int ny = y + dy[i];
                     int nx = x + dx[i];
                     if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-                    int nd = w + a[ny][nx];
-                    if(dist[ny][nx] > nd){
-                        dist[ny][nx] = nd;
-                        pq.offer(new Data(ny, nx, nd));
+                    int nw = w + a[ny][nx];
+                    if(dist[ny][nx] > nw){
+                        dist[ny][nx] = nw;
+                        pq.offer(new Node(ny, nx, nw));
                     }
                 }
             }
@@ -49,9 +52,9 @@ public class Main {
         }
     }
 
-    static class Data{
+    static class Node{
         int y, x, w;
-        Data(int y, int x, int w){
+        Node(int y, int x ,int w){
             this.y = y;
             this.x = x;
             this.w = w;

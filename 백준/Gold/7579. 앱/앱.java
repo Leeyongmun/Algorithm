4cf3179657d2f8e407dc5b7a780554053e64fc13
Leeyongmun <cocoyi000319@gmail.com>
2025-09-14 18:11:22 +0900

    [Gold III] Title: 앱, Time: 100 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,39 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[] a;
-    static int[] cost;
-    static int[] dp;
+    static int[] b, c;
+    static int[][] dp;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        a = new int[n + 1];
-        cost = new int[n + 1];
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        b = new int[n + 1];
+        c = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
-            a[i] = sc.nextInt();
+            b[i] = Integer.parseInt(st.nextToken());
         }
-        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        int costSum = 0;
         for(int i = 1; i <= n; i++){
-            cost[i] = sc.nextInt();
-            sum += cost[i];
+            c[i] = Integer.parseInt(st.nextToken());
+            costSum += c[i];
         }
-        dp = new int[sum + 1];
+        dp = new int[n + 1][costSum + 1];
         for(int i = 1; i <= n; i++){
-            for(int j = sum; j >= cost[i]; j--){
-                dp[j] = Math.max(dp[j], dp[j - cost[i]] + a[i]);
+            for(int j = 0; j <= costSum; j++){
+                dp[i][j] = dp[i - 1][j];
+                if(j >= c[i]){
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - c[i]] + b[i]);
+                }
             }
         }
-        int ret = 0;
-        for(int i = 0; i <= sum; i++){
-            if(dp[i] >= m){
-                ret = i;
+        for(int i = 0; i <= costSum; i++){
+            if(dp[n][i] >= m){
+                System.out.println(i);
                 break;
             }
         }
-        System.out.println(ret);
     }
 }

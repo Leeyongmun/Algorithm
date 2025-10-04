a029dec644f84c25932958453c905c808f3a3041
Leeyongmun <cocoyi000319@gmail.com>
2025-10-04 19:19:51 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<algorithm> 
+#include<vector> 
+#include<tuple>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+	cin >> n;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 0;
+	dp[4] = 1;
+	dp[5] = 1;
+
+	for (int i = 6; i <= n; i++) {
+		if (dp[i - 1] == 0 || dp[i - 3] == 0 || dp[i - 4] == 0) dp[i] = 1;
+		else dp[i] = 0;
+	}
+	if (dp[n] == 0) cout << "CY";
+	else cout << "SK";
+}

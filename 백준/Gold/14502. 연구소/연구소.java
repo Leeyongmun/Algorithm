7066a4fdc6e7a5f8f0a4ede749b5c31f3b2b05d1
Leeyongmun <cocoyi000319@gmail.com>
2025-10-09 18:14:26 +0900

    [Gold IV] Title: 연구소, Time: 716 ms, Memory: 293980 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,86 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
     static int[][] a;
-    static int[][] b;
+    static ArrayList<Pair> list = new ArrayList<>();
     static ArrayList<Pair> virus = new ArrayList<>();
-    static ArrayList<Pair> empty = new ArrayList<>();
-    static ArrayList<Pair> tmp = new ArrayList<>();
-    static boolean[][] visited;
     static int[] dy = { -1, 0, 1, 0};
-    static int[] dx = { 0, 1, 0, -1};
+    static int[] dx = {0, 1, 0, -1};
     static int ret = 0;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        a = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                a[i][j] = sc.nextInt();
-                if(a[i][j] == 0){
-                    empty.add(new Pair(i, j));
-                }
-                else if(a[i][j] == 2){
-                    virus.add(new Pair(i, j));
-                }
-            }
-        }
-        solve(0);
-        System.out.println(ret);
-    }
-
-    private static void solve(int now){
-        if(tmp.size() == 3){
-            b = new int[n][m];
-            for(int i = 0; i < n; i++){
-                b[i] = a[i].clone();
-            }
-            for(Pair p : tmp){
-                b[p.y][p.x] = 1;
-            }
-            visited = new boolean[n][m];
-            for(Pair p : virus){
-                dfs(p.y, p.x);
-            }
-            int sum = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(b[i][j] == 0){
-                        sum++;
-                    }
-                }
-            }
-            ret = Math.max(ret, sum);
-            return;
-        }
-        for(int i = now; i < empty.size(); i++){
-            tmp.add(empty.get(i));
-            solve(i + 1);
-            tmp.remove(tmp.size() - 1);
-        }
-    }
-
-    private static void dfs(int y, int x){
-        visited[y][x] = true;
-        b[y][x] = 2;
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if(visited[ny][nx] || b[ny][nx] != 0) continue;
-            dfs(ny, nx);
-        }
-    }
-
+    
     static class Pair{
         int y, x;
         Pair(int y, int x){
             this.y = y;
             this.x = x;
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                a[i][j] = Integer.parseInt(st.nextToken());
+                list.add(new Pair(i, j));
+                if(a[i][j] == 2){
+                    virus.add(new Pair(i, j));
+                }
+            }
+        }
+        solve(0, 0);
+        System.out.println(ret);
+    }
+
+    private static void solve(int cnt, int now){
+        if(cnt == 3){
+            bfs();
+            return;
+        }
+        for(int i = now; i < list.size(); i++){
+            Pair cur = list.get(i);
+            if(a[cur.y][cur.x] != 0) continue;
+            a[cur.y][cur.x] = 1;
+            solve(cnt + 1, now + 1);
+            a[cur.y][cur.x] = 0;
+        }
+    }
+
+    private static void bfs(){
+        int[][] b = new int[n][m];
+        for(int i = 0; i < n; i++){
+            b[i] = a[i].clone();
+        }
+        Queue<Pair> q = new ArrayDeque<>();
+        for(int i = 0; i < virus.size(); i++){
+            q.offer(virus.get(i));
+        }
+        while(!q.isEmpty()){
+            Pair cur = q.poll();
+            int y = cur.y;
+            int x = cur.x;
+            for(int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if(b[ny][nx] != 0) continue;
+                b[ny][nx] = 2;
+                q.offer(new Pair(ny, nx));
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(b[i][j] == 0){
+                    sum++;
+                }
+            }
+        }
+        ret = Math.max(sum, ret);
     }
 }

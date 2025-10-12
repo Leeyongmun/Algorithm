36ff0873c04d3d61ac115b2bbc06949dc91fe832
Leeyongmun <cocoyi000319@gmail.com>
2025-10-12 23:00:44 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,21 @@
+#include<iostream> 
+#include<algorithm> 
+#include<vector> 
+#include<string>
+#include<queue>
+#include<unordered_set>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+	cin >> n;
+	dp[1] = 1;
+	dp[3] = 1;
+	dp[4] = 1;
+	for (int i = 5; i <= n; i++) {
+		dp[i] = !dp[i - 1] || !dp[i - 3] || !dp[i - 4];
+	}
+	cout << (dp[n] ? "SK" : "CY");
+}

3d415cb8416f32cf78e0b288a9b300b5c0eebc73
Leeyongmun <cocoyi000319@gmail.com>
2025-12-05 20:59:51 +0900

    [Gold III] Title: 이진 트리, Time: 136 ms, Memory: 10216 KB -BaekjoonHub

--- 백준/Gold/13325. 이진 트리/이진 트리.cc
+++ 백준/Gold/13325. 이진 트리/이진 트리.cc
@@ -7,41 +7,31 @@
 using namespace std;
 
 int n;
-vector<int> tree, dp;
+vector<int> tree;
 int ret = 0;
 int p = 0;
+int len = 0;
 
 int dfs(int cur) {
-	if (cur >= pow(2, n)) {
-		return tree[cur];
-	}
-	int left = dfs(cur * 2);
-	int right = dfs(cur * 2 + 1);
-	if (right > left) {
-		p += right - left;
-		left += right - left;
-	}
-	else {
-		p += left - right;
-		right += left - right;
-	}
-	return left + tree[cur];
+	if (cur * 2 > len - 1) return 0;
+	int left = dfs(cur * 2) + tree[cur * 2];
+	int right = dfs(cur * 2 + 1) + tree[cur * 2 + 1];
+	ret += abs(right - left);
+	return max(left, right);
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin >> n;
-	int len = 2 * pow(2, n);
+	len = 1 << (n + 1);
 	tree.resize(len);
-	dp.resize(len);
-	tree[1] = 0;
 	int sum = 0;
 	for (int i = 2; i < len; i++) {
 		cin >> tree[i];
 		sum += tree[i];
 	}
 	dfs(1);
-	cout << sum + p;
+	cout << sum + ret;
 }
 

ef5da500cd559163768a9bc08adca36c07e4136f
Leeyongmun <cocoyi000319@gmail.com>
2026-02-25 21:35:22 +0900

    [Bronze V] Title: CAPS, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <string>
+#include <tuple>
+#include <climits>
+using namespace std;
+
+string s;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		s[i] = s[i] - ('a' - 'A');
+	}
+	cout << s;
+}

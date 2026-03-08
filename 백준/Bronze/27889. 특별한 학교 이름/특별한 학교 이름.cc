c85fc0c4fecc60af98a06650c3c4e9db7bd10133
Leeyongmun <cocoyi000319@gmail.com>
2026-03-09 01:02:47 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+string s;
+
+int main(){
+    cin >> s;
+    
+    if(s == "NLCS") cout << "North London Collegiate School";
+    else if(s == "BHA") cout << "Branksome Hall Asia";
+    else if(s == "KIS") cout << "Korea International School";
+    else cout << "St. Johnsbury Academy";
+}

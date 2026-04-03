e4ed19271644ea81adfe4c8921000ba4cf0703c3
Leeyongmun <cocoyi000319@gmail.com>
2026-04-03 23:20:46 +0900

    [Gold V] Title: DKSH를 찾아라, Time: 0 ms, Memory: 3588 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Gold/25427. DKSH를 찾아라/DKSH를 찾아라.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <cstring>
+using namespace std;
+
+int n;
+string s;
+
+int H[100001];
+int SH[100001];
+int KSH[100001];
+int DKSH[100001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> s;
+	for (int i = s.size() - 1; i >= 0; i--) {
+		H[i] = H[i + 1];
+		SH[i] = SH[i + 1];
+		KSH[i] = KSH[i + 1];
+		DKSH[i] = DKSH[i + 1];
+		if (s[i] == 'H') {
+			H[i]++;
+		}
+		else if (s[i] == 'S') {
+			SH[i] += H[i + 1];
+		}
+		else if (s[i] == 'K') {
+			KSH[i] += SH[i + 1];
+		}
+		else if (s[i] == 'D') {
+			DKSH[i] += KSH[i + 1];
+		}
+	}
+	cout << DKSH[0];
+}
